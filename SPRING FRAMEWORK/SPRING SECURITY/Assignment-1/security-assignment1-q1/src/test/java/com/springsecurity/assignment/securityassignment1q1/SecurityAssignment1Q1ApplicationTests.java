d7026b3f5c81a214441c8da07be26fabd215fcc6
nikitas19 <86960160+nikitas19@users.noreply.github.com>
2021-08-02 17:36:08 +0530

    Add files via upload

---
+++ SPRING FRAMEWORK/SPRING SECURITY/Assignment-1/security-assignment1-q1/src/test/java/com/springsecurity/assignment/securityassignment1q1/SecurityAssignment1Q1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsecurity.assignment.securityassignment1q1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityAssignment1Q1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
